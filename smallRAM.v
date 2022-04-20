36d76f54df5242547385511e4a0a1367215e4e4c
Zach-Bravo <98495104+Zach-Bravo@users.noreply.github.com>
2022-04-19 20:06:36 -0400

    Update smallRAM.v

--- smallRAM.v
+++ smallRAM.v
@@ -2,35 +2,33 @@ module smallRAM(rw, dataIn, CS, address, clk, dataOut);
 
 	input rw;
 	input [7:0] dataIn;
-	//input [29:0] address;
 	input [5:0] address;
 	input CS;
 	input clk;
 	
-	wire en = 1;
+	//wire en = 1;
 
 	parameter adr_width=6;
-	//parameter adr_width=30;
-
+	
 	parameter ram_depth= 1 << adr_width;
 
-	reg[7:0]memOut;
+	//reg[7:0]memOut;
 	reg[7:0]mem[0:ram_depth-1];
 
 	output [7:0] dataOut;
 
 
-	always@(en) begin 
+	always@(rw) begin 
 	if(CS && rw)  //Write
 	mem[address] = dataIn;
 	end
 
 
-	always@(clk) begin 
+	/*always@(rw) begin 
 	memOut = mem[address]; //Read
-	end
+	end*/
 
-	assign dataOut = CS?memOut:8'b0;
+	assign dataOut = CS?mem[address]:8'b0;
 
 
 endmodule 
