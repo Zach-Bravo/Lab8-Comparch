3ebed3c7ef861fd57815aeb30ac3882318f89128
Zach-Bravo <98495104+Zach-Bravo@users.noreply.github.com>
2022-04-20 22:59:00 -0400

    Update full_datapath.v

--- full_datapath.v
+++ full_datapath.v
@@ -3,11 +3,10 @@ module full_datapath(clk, reset, opcode, ALUsrc, RegWrite,
 
 //Input CU Controls: RegWrite, ALUsrc, MemRW, PCsrc, MemToReg
 
-//wire [31:0] address;
 input [3:0] opcode;
 input RegWrite, MemRW, MemtoReg, PCsrc, ALUsrc; //1 or 0
 
-output [31:0] PC_next; //May Replace As Needed
+output [31:0] PC_next; 
 output [31:0] finalout;
 
 input clk, reset;
@@ -35,10 +34,6 @@ wire [3:0] status;
 
 
 wire [31:0] RAMout;
-wire [7:0] RAMout_small1;
-wire [7:0] RAMout_small2;
-wire [7:0] RAMout_small3;
-wire [7:0] RAMout_small4;
 
 //PC Mux potNext1 & potNext2 (updated + Jump) -> PC_out
 wire [31:0] potNext1;
@@ -71,23 +66,14 @@ read2_Mux u7(ALUsrc, RS2_full, IMM_full, Read2);
 //ALU(A, B, opcode, STATUS, FINAL,Cin); status[2] = zero value
 ALU u8(Read1, Read2, opcode, status, result, 0);
 
-//module RAM (Din, address, Dout, clk, rw);
-//Used to be RAM u8(result, RS2_full, RAMout, clk, MemRW);
-RAM u9(RS2_full, result[31:24], RAMout_small1, clk, MemRW);
-RAM u10(RS2_full, result[23:16], RAMout_small2, clk, MemRW);
-RAM u11(RS2_full, result[15:8], RAMout_small3, clk, MemRW);
-RAM u12(RS2_full, result[7:0], RAMout_small4, clk, MemRW);
-
-assign RAMout[31:24] = RAMout_small1;
-assign RAMout[23:16] = RAMout_small2;
-assign RAMout[15:8]  = RAMout_small3;
-assign RAMout[7:0]   = RAMout_small4;
+//RAM (Din,      address,     Dout,   clk,    rw);
+RAM u9(RS2_full, result[7:0], RAMout, clk, MemRW);
 
 //Mux -> MemMux
 mem_Mux u13(MemtoReg, result, RAMout, finalout);
 
 
-//Adder -> Jump
+//Adder -> Jump/Branch
 FullAdder u14(PC_address, IMM_full, 0, potNext2, status);
 
 //Mux -> Next PCout aka PC_next
